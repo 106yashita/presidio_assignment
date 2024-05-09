f6eeceec685e3113364d43e25bf3ef7002d2c241
yashita <yashitakhandelwal8@gmail.com>
2024-05-09 17:50:04 +0530

    day21 Assignemnt

--- day20/DoctorPatienceSolution/DoctorPatienceApp/obj/Debug/net8.0/DoctorPatientApp.AssemblyInfo.cs
+++ day20/DoctorPatienceSolution/DoctorPatienceApp/obj/Debug/net8.0/DoctorPatientApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoctorPatientApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fef4424dd5d587bedaf18aba0a08d4eda7f6276")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75b791a96ce075ffeaa04d77c28102248f0ed9a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoctorPatientApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DoctorPatientApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
